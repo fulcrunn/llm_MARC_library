a5b3e9b1c905a0561323b1c646a0ed8987adfc49
fulcrunn <fulcrunn@yahoo.com.br>
2026-02-24 23:04:25 -0300

    Train correction, exclude bitsandbytes

--- train.py
+++ train.py
@@ -63,17 +63,19 @@ print("Eval size:", len(dataset["test"]))
 # Quantização 4-bit (QLoRA)
 # =====================================================
 
+'''
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.float16,
     bnb_4bit_use_double_quant=True,
 )
+'''
 
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME,
-    quantization_config=bnb_config,
     device_map="auto",
+    torch_dtype=torch.float16,
     trust_remote_code=True,
 )
 
