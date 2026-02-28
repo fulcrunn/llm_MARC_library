352d24dfde15574673241f805db60a1366bfad52
fulcrunn <fulcrunn@yahoo.com.br>
2026-02-27 21:40:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ set -e
 echo "🔄 Atualizando sistema e instalando dependências base..."
 apt-get update
 apt-get install -y git python3-pip python3-dev ninja-build build-essential wget
+apt-get install gdown -y
+gdown --fuzzy "https://drive.google.com/file/d/1p_N-exEkaDAmFNODdFKKqLER8shw2_nG/view?usp=drive_link"
+gdown --fuzzy "https://drive.google.com/file/d/1uFQcfFqaPzXtuYObg719yQ-czRUXhXDM/view?usp=drive_link"
 
 echo "📥 Clonando repositório..."
 if [ ! -d "llm_MARC_library" ]; then
